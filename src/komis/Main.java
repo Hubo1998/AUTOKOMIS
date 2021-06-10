486d1d23f1b506b4d716f258830b14a89d63c7a6
Hubo90 <hmarchwat@gmail.com>
2021-06-10 15:46:45 +0200

    Początki bazy pojazdów, generowanie modeli i marek pojazdów.

--- src/komis/Main.java
+++ src/komis/Main.java
@@ -18,5 +18,16 @@ public class Main {
 		int x =rand.nextInt(10)+Mechanic.Janusz.price;
 		System.out.println(x);
 	System.out.println(Player.player1.cash);
+	String[] arr=new String[5];
+	for (int i=0;i<5;i++){
+		arr[i]='"'+String.valueOf(i)+'"';
+		System.out.println(arr[i]);
+	}
+	//System.out.println(Generate.generateVehicle());
+	//System.out.println(Generate.models[Generate.genModel(10)]);
+	Generate.genModel(4);
+	Car car122=Generate.generateVehicle();
+	System.out.println(car122.yearOfProduction+car122.model+car122.producer);
+	car122.repair("brakes",Mechanic.Janusz,Player.player1);
     }
 }
