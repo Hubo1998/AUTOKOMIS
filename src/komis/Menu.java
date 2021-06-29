76836e2080a11b73e6d70087c5c3e6297c050b57
Hubo90 <hmarchwat@gmail.com>
2021-06-29 19:04:08 +0200

    obsługa wyjątków v2

--- src/komis/Menu.java
+++ src/komis/Menu.java
@@ -50,35 +50,19 @@ public class Menu {
             System.out.println("1. Przeglądaj bazę samochodów do kupienia\n2. Przeglądaj bazę posiadanych samochodów\n3. Przejrzyj potencjalnych klientów\n4. Kup reklamę\n5. Sprawdź stan konta\n6. Pokaż historię transakcji");
             String inputfirstpage = scanner.nextLine();
             switch (inputfirstpage) {
-                case "1": {
-                    showCarBase(player);
-                    break;
-                }
-                case "2": {
-                    checkGarageStatus(player);
-                    break;
-                }
-                case "3": {
-                    player.viewBaseOfClients();
-                    break;
-                }
-                case "4": {
-                    buyAdvertisement(player);
-                    break;
-                }
-                case "5": {
-                    player.checkCash();
-                    break;
-                }
-                case "6":{
-                    player.showHistory();
-                    break;
-                }
-                case "exit": {
+                case "1" -> showCarBase(player);
+                case "2" -> checkGarageStatus(player);
+                case "3" -> player.viewBaseOfClients();
+                case "4" -> buyAdvertisement(player);
+                case "5" -> player.checkCash();
+                case "6" -> player.showHistory();
+                case "exit" -> {
                     return false;
                 }
-                default:
+                default -> {
+                    System.out.println("Błąd");
                     return true;
+                }
             }
         }
         return true;
@@ -102,11 +86,17 @@ public class Menu {
             }
             case "3" -> {
                 System.out.println("Wpisz numer samochodu, który chciałbyś sprawdzić:");
-                int inputcheckcar = inputWithMinusOne();
-                System.out.println(player.baseOfCars.get(inputcheckcar).producer+" "+player.baseOfCars.get(inputcheckcar).model);
-                player.baseOfCars.get(inputcheckcar).carPartsStatus();
+                try{
+                    int inputcheckcar = inputWithMinusOne();
+                    System.out.println(player.baseOfCars.get(inputcheckcar).producer + " " + player.baseOfCars.get(inputcheckcar).model);
+                    player.baseOfCars.get(inputcheckcar).carPartsStatus();
+                }catch (NumberFormatException exception){
+                    System.out.println("Musisz wpisać numer samochodu");
+                }catch (IndexOutOfBoundsException exception){
+                    System.out.println("Samochód spoza zakresu");
+                }
             }
-            default -> {}
+            default -> System.out.println("Błąd");
         }
     }
     public void checkGarageStatus(Player player){
@@ -120,37 +110,55 @@ public class Menu {
             switch (inputcheckgarage) {
                 case "2" -> {
                     System.out.println("Wpisz numer samochodu,który chcesz sprawdzić");
-                    int inputcheckcar = inputWithMinusOne();
-                    System.out.println(player.playerGarage.get(inputcheckcar).producer + " " + player.playerGarage.get(inputcheckcar).model);
-                    player.playerGarage.get(inputcheckcar).carPartsStatus();
+                    try{
+                        int inputcheckcar = inputWithMinusOne();
+                        System.out.println(player.playerGarage.get(inputcheckcar).producer + " " + player.playerGarage.get(inputcheckcar).model);
+                        player.playerGarage.get(inputcheckcar).carPartsStatus();
+                    }catch (NumberFormatException exception){
+                        System.out.println("Musisz wpisać numer samochodu");
+                    }catch (IndexOutOfBoundsException exception){
+                        System.out.println("Samochód spoza zakresu");
+                    }
                     System.out.println("W takim stanie jest Twoje auto");
                 }
                 case "3" -> {
                     System.out.println("Które auto chciałbyś naprawić? Wybierz numer auta.");
-                    int inputcartorepair= inputWithMinusOne();
-                    System.out.println(player.playerGarage.get(inputcartorepair).producer + " " + player.playerGarage.get(inputcartorepair).model);
-                    player.playerGarage.get(inputcartorepair).carPartsStatus();
-                    System.out.println("Który podzespół chciałbyś naprawić?");
-                    String inputparttorepair = scanner.nextLine();
-                    switch (inputparttorepair) {
-                        case "1" -> Mechanicchoice(Components.Comps.brakes, inputcartorepair, player);
-                        case "2" -> Mechanicchoice(Components.Comps.suspension, inputcartorepair, player);
-                        case "3" -> Mechanicchoice(Components.Comps.engine, inputcartorepair, player);
-                        case "4" -> Mechanicchoice(Components.Comps.body, inputcartorepair, player);
-                        case "5" -> Mechanicchoice(Components.Comps.gearbox, inputcartorepair, player);
-                        default -> System.out.println("Coś nie teges");
+                    try{
+                        int inputcartorepair = inputWithMinusOne();
+                        System.out.println(player.playerGarage.get(inputcartorepair).producer + " " + player.playerGarage.get(inputcartorepair).model);
+                        player.playerGarage.get(inputcartorepair).carPartsStatus();
+                        System.out.println("Który podzespół chciałbyś naprawić?");
+                        String inputparttorepair = scanner.nextLine();
+                        switch (inputparttorepair) {
+                            case "1" -> Mechanicchoice(Components.Comps.brakes, inputcartorepair, player);
+                            case "2" -> Mechanicchoice(Components.Comps.suspension, inputcartorepair, player);
+                            case "3" -> Mechanicchoice(Components.Comps.engine, inputcartorepair, player);
+                            case "4" -> Mechanicchoice(Components.Comps.body, inputcartorepair, player);
+                            case "5" -> Mechanicchoice(Components.Comps.gearbox, inputcartorepair, player);
+                            default -> System.out.println("Coś nie teges");
+                        }
+                    }catch (NumberFormatException exception){
+                        System.out.println("Musisz wpisać numer samochodu");
+                    }catch (IndexOutOfBoundsException exception){
+                        System.out.println("Samochód spoza zakresu");
                     }
                 }
                 case "4" -> {
                     System.out.println("Które auto chciałbyś sprzedać?");
-                    int inputcartosell = inputWithMinusOne();
-                    System.out.println(player.playerGarage.get(inputcartosell).producer + " " + player.playerGarage.get(inputcartosell).model + " za " + Math.round(player.playerGarage.get(inputcartosell).finalPrice));
-                    System.out.println("Komu chciałbyś sprzedać auto?");
-                    player.viewBaseOfClients();
-                    int inputclienttosell = inputWithMinusOne();
-                    player.sellInHarmonyWithMoodOfClient(inputcartosell, player.baseOfClients.get(inputclienttosell));
+                    try{
+                        int inputcartosell = inputWithMinusOne();
+                        System.out.println(player.playerGarage.get(inputcartosell).producer + " " + player.playerGarage.get(inputcartosell).model + " za " + Math.round(player.playerGarage.get(inputcartosell).finalPrice));
+                        System.out.println("Komu chciałbyś sprzedać auto?");
+                        player.viewBaseOfClients();
+                        int inputclienttosell = inputWithMinusOne();
+                        player.sellInHarmonyWithMoodOfClient(inputcartosell, player.baseOfClients.get(inputclienttosell));
+                    }catch (NumberFormatException exception){
+                        System.out.println("Musisz wpisać numer");
+                    }catch (IndexOutOfBoundsException exception){
+                        System.out.println("Spoza zakresu");
+                    }
                 }
-                default -> {}
+                default -> System.out.println("Błąd");
             }
         }
     }
@@ -163,12 +171,18 @@ public class Menu {
             System.out.println(player.playerGarage.get(cartorepair).nameOfComp(comps)+" cena u Mariana ("+Mechanic.Marian.guarantee+"% gwarancji, że się uda, "+Mechanic.Marian.brokelse+"% szans na zepsucie czegoś innego cena między "+Math.round(player.playerGarage.get(cartorepair).price*player.playerGarage.get(cartorepair).valueOf(comps)*Mechanic.Marian.price*0.01)+" - "+Math.round(player.playerGarage.get(cartorepair).price*player.playerGarage.get(cartorepair).valueOf(comps)*(Mechanic.Marian.price+10)*0.01));
             System.out.println(player.playerGarage.get(cartorepair).nameOfComp(comps)+" cena u Adriana ("+Mechanic.Adrian.guarantee+"% gwarancji, że się uda, "+Mechanic.Adrian.brokelse+"% szans na zepsucie czegoś innego cena między "+Math.round(player.playerGarage.get(cartorepair).price*player.playerGarage.get(cartorepair).valueOf(comps)*Mechanic.Adrian.price*0.01)+" - "+Math.round(player.playerGarage.get(cartorepair).price*player.playerGarage.get(cartorepair).valueOf(comps)*(Mechanic.Adrian.price+10)*0.01));
             System.out.println("Przemyśl to dobrze, jak się nie uda, trzeba będzie jechać do Janusza");
-            String inputmechanicchoice=scanner.nextLine();
-            switch(inputmechanicchoice){
-                case "1"->repairTime(Mechanic.Janusz,comps,player.playerGarage.get(cartorepair),player);
-                case "2"->repairTime(Mechanic.Marian,comps,player.playerGarage.get(cartorepair),player);
-                case "3"->repairTime(Mechanic.Adrian,comps,player.playerGarage.get(cartorepair),player);
-                default -> System.out.println("Coś nie teges");
+            try{
+                String inputmechanicchoice = scanner.nextLine();
+                switch (inputmechanicchoice) {
+                    case "1" -> repairTime(Mechanic.Janusz, comps, player.playerGarage.get(cartorepair), player);
+                    case "2" -> repairTime(Mechanic.Marian, comps, player.playerGarage.get(cartorepair), player);
+                    case "3" -> repairTime(Mechanic.Adrian, comps, player.playerGarage.get(cartorepair), player);
+                    default -> System.out.println("Coś nie teges");
+                }
+            }catch (NumberFormatException exception){
+                System.out.println("Musisz wpisać numer samochodu");
+            }catch (IndexOutOfBoundsException exception){
+                System.out.println("Samochód spoza zakresu");
             }
         }
     }
